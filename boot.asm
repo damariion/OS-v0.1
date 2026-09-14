f970dd488025859727045a076d2b60a8ed153e13
Damarion Abendanon <da.abendanon@gmail.com>
2026-09-15 00:07:31 +0200

    Read disk through interrupt

--- boot.asm
+++ boot.asm
@@ -1,6 +1,17 @@
 BITS 16
 ORG  0x7C00
 
+%%macro console.write 1
+    lodsb
+    cmp al, 0
+    je .done
+    mov ah, 0xE
+    mov bx, 0
+    mov si, %1
+    int 0x10
+    jmp console.write
+%endmacro
+
     cli
     xor ax, ax
     mov ds, ax
@@ -17,21 +28,9 @@ main:
     mov bx, buffer
     call disk.read
 
-    mov si, buffer
-    call console.write
+    console.write buffer
 
     jmp $
-
-console.write:;(input->si)
-    lodsb
-    cmp al, 0
-    je .done
-    mov ah, 0xE
-    mov bx, 0
-    int 0x10
-    jmp console.write
-.done:
-    ret
 
 disk.read:;(sector->cl, output->bx)
     mov ah, 0x2
